393fc54377f03df70fb80498764df04cce19c20a
Keshaw Soni <keshawsoni08@gmail.com>
2020-11-29 17:12:24 +0530

    Added my Python Exercise

---
+++ exercise.py
@@ -0,0 +1,60 @@
+ -*- coding: utf-8 -*-
+
+Spyder Editor
+
+This is a temporary script file.
+
+
+n = int(input("a no. "))
+
+x = ((n + 2)*3 - 6)/3
+print(x)
+
+print(7 + 7 + 7 + 7)
+
+my_int = 5
+my_int += 3
+print(my_int)
+
+print(4//5)
+
+print(30 - 3 ** 2 + 8 // 3 ** 2 * 10)
+
+print((2**2)**3)
+
+n = input("a no. ")
+n_int = int(n)
+n_float = float(n)
+n_str = str(n)
+
+print(n_int)
+print(n_float)
+print(n_str)
+
+a = str(input("a = "))
+b = str(input("b = "))
+c = str(input("c = "))
+
+print((a + b)*c)
+
+n = int(input("n = "))
+if(n%2 == 1):
+    print("1")
+elif(n%2 == 0):
+    print("0")
+    
+import turtle
+
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
