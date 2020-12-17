e1a4bf149fec97e74738ef2bf361e06358f6d72d
Keshaw Soni <keshawsoni08@gmail.com>
2020-12-17 21:49:14 +0530

    ExerciseDEC.py

--- ExerciseDEC.py
+++ ExerciseDEC.py
@@ -295,3 +295,9 @@ y = [1,4,9,16,25,36,49,64,81,100]
 
 plt.plot(x,y, linestyle = 'steps',animated = 'true')
 plt.show()
+
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [1,9,1,9,1,9,1,9,1,9]
+
+plt.plot(x,y,'o-b', ms = 10, mfc = 'red', mec = 'black', linestyle = ':')
+plt.show()
