4f1747dad72742af1dcf30281355719b375bc047
Keshaw Soni <keshawsoni08@gmail.com>
2021-01-02 23:26:20 +0530

    ExerciseDEC.py

--- ExerciseDEC.py
+++ ExerciseDEC.py
@@ -543,3 +543,17 @@ elif(passrating > 90):
     print("good")
 elif(passrating > 95):
     print("great")
+
+'''Find the two-digit number such that when you square it, the resulting three-digit
+number has its rightmost two digits the same as the original two-digit number. That is,
+for a number in the form AB, AB*AB = CAB for some C.'''
+
+n = int(input("Tell me a two digit no. = "))
+
+z = n*n 
+k = z%100
+
+if(k == n):
+    print("Yes",k)
+else:
+    print("you are wrong")
