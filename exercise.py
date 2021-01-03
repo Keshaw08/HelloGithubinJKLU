48ea7ab9e24a8b50ba933b3634754aae28639012
Keshaw Soni <keshawsoni08@gmail.com>
2021-01-03 23:04:52 +0530

    exercise.py

--- exercise.py
+++ exercise.py
@@ -446,3 +446,8 @@ if(n%3==0):
     print("yes it is divisible")
 else:
     print("no it is not")
+
+A_int = int(input('Enter an integer greater than 10: '))
+while A_int > 10:
+    A_int = A_int - 1
+    print(A_int)
