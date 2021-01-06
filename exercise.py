e6fc6c66ac9572724157327e96d186ab2eb7956a
Keshaw Soni <keshawsoni08@gmail.com>
2021-01-06 21:46:39 +0530

    exercise.py

--- exercise.py
+++ exercise.py
@@ -509,3 +509,31 @@ plt.xlabel("BsmtSF from 700 to 800")
 plt.show()
 
 input("tell me file name ")
+
+file_str = input("Open what file: ")
+find_line_str = input("Which line (integer) :")
+
+try:
+    input_file = open(file_str)
+    find_line_int = int(find_line_str)
+    line_count_int = 1
+    for line_str in input_file:
+        if line_count_int == find_line_int:
+            print("Line {} of file {} is {}".format(find_line_int, file_str,line_str))
+            break
+        line_count_int += 1
+    else:
+        print("Line {} of file {} not found".format(file_line_int, file_str))
+    input_file.close()
+except IOError:
+    print("The file",file_str,"doesn't exist.")
+    
+except ValueError:
+    print("Line", find_line_str, "isn't a legal line number.")
+    
+print("End of the program")
+
+
+
+
+
