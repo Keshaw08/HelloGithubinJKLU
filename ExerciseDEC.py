fd8a039759e7f883528e3b9d48b4f40c7f8eda35
Keshaw Soni <keshawsoni08@gmail.com>
2021-01-07 21:45:41 +0530

    ExerciseDEC.py

--- ExerciseDEC.py
+++ ExerciseDEC.py
@@ -616,3 +616,16 @@ def einstein(mass):
     return E
 
 einstein(200)
+
+def func1(str1,str2):
+    if(str1 > str2):
+        result_str = str1[1:]
+    else:
+        result_str = str2[:-1]
+    return result_str
+
+response1_str = input("Enter a string: ")
+response2_str = input("Enter a second string: ")
+
+print(func1(response1_str, response2_str))
+print(func1(response2_str,response1_str))
