9c24f286ce60c8a4dbc34363796ceb055b706c7e
Keshaw08 <keshawsoni08@gmail.com>
2021-01-27 20:08:13 +0530

    exercise.py

--- exercise.py
+++ exercise.py
@@ -863,3 +863,26 @@ set2 = {"cd"}
 print(f1(set1,set2))
 print(f1(set1,set2,1))
 print(f1(set1,set2,2))
+
+def f1 (my_dict):
+    temp = 0
+    for value in my_dict.values():
+        temp = temp + value
+        
+def f2 (my_dict):
+    temp = ""
+    for key in my_dict:
+        if temp> key:
+            temp = key
+    return temp
+
+def f3 (my_dict,k,v):
+    if k in my_dict:
+        my_dict[k]=v
+
+a_dict = {"bill":1, "rich":2,"fred":10,"walter":20}
+
+#print(f1(a_dict))
+#print(f2(a_dict))
+print(None == f3(a_dict,"bill",-1))
+#print(a_dict)
