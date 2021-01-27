3856e7a2d9c1b595ea233f3108e20fd6e2085b39
Keshaw08 <keshawsoni08@gmail.com>
2021-01-27 20:16:15 +0530

    exercise.py

--- exercise.py
+++ exercise.py
@@ -916,3 +916,15 @@ while number > 0:
     count = count + 1
 print("Count is : ",count)
 print("Number is : ",number)
+
+user_str = input("Enter a positive integer:") # Line 1
+my_int = int(user_str)
+count = 0
+while my_int > 0:
+    if my_int % 2 == 1:
+        my_int = my_int//2
+    else:
+        my_int = my_int - 1
+    count = count + 1 # Line 2
+print(count) # Line 3
+print(my_int) # Line 4
