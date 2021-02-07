d6f478c4338ca951d4cd5ba5fd550cf56fda2938
Keshaw08 <keshawsoni08@gmail.com>
2021-02-07 22:06:19 +0530

    exercise.py

--- exercise.py
+++ exercise.py
@@ -1136,3 +1136,10 @@ for i in L:
     s = s + str(i)
     
 print(s)
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n1 = int(input())
+n2 = int(input())
+print(n1//n2)
+print(n1%n2)
+print(divmod(n1,n2))
