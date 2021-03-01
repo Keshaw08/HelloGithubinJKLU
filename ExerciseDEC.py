05518df2e36090b5a6718b68f729873c8bf08c0a
Keshaw08 <keshawsoni08@gmail.com>
2021-03-01 22:09:06 +0530

    ExerciseDEC.py

--- ExerciseDEC.py
+++ ExerciseDEC.py
@@ -840,3 +840,7 @@ n = int(input("Tell me a number : "))
 for i in range(1,n+1):
     if (n%i==0):
         print(i)
+
+n = int(input("Tell me a number : "))
+z = [[n%i==0] for i in range(1,n+1)]
+print(z)
