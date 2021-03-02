62bcca55969d9bb6f8f5505c62451d2bfc83c5be
Keshaw08 <keshawsoni08@gmail.com>
2021-03-02 21:58:47 +0530

    ExerciseDEC.py

--- ExerciseDEC.py
+++ ExerciseDEC.py
@@ -889,3 +889,22 @@ def listchng(list1):
         return list2
 
 listchng(list1)
+
+'''It is oftentimes advantageous to be able to transfer data between multiple lists while
+rearranging their order. For instance, say that list1 = [1,2,3,4,5,6,7,8,9]
+and you wish to add the numbers in the index range 4:7 of list1 to another
+list, list2, in reverse order while simultaneously removing them from list1. If
+list2 = [100,200], the result will be list2 = [100,200,7,6,5]. Write a
+function named transform that takes as arguments list1, list2, r1, and r2,
+that removes items from list1 in the slice r1:r2, appends them onto list2 in
+reverse order, and returns the resulting list. For example, in this case, the function call
+will be transform(list1, list2, 4,7).'''
+list1 = [1,2,3,4,5,6,7,8,9]
+list2 = [100,200]
+
+def transform(list1,list2,r1,r2):
+    for i in range(r1,r2):
+        list2.append(list1[i])
+    print(list2)
+    
+transform(list1,list2,4,7)
