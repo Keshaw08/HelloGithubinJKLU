bd93f5c67bc47ad621b93c74c0f0e34a58df91e5
Keshaw08 <keshawsoni08@gmail.com>
2021-03-04 23:07:08 +0530

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+
+	public static void main(String arg[]){
+	
+		System.out.println("Hello World!");
+		System.out.println("1st Java Program");
+		System.out.println("I am Keshaw :)");
+	}
+}
