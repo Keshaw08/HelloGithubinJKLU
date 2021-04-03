9f9991b0164423db56e6f69cc36c3031d374dca0
Keshaw08 <keshawsoni08@gmail.com>
2021-04-03 23:18:38 +0530

    exer_java.java

--- exer_java.java
+++ exer_java.java
@@ -102,3 +102,19 @@ public class nothing{
 	}
 
 }
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner scan = new Scanner(System.in);
+            for (int i = 1; scan.hasNext() == true ; i++){
+                System.out.println(i + " " + scan.nextLine());
+            }
+                
+            
+    }
+}
