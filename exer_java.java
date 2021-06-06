366d596b8dadac15c7c80d5d111aff7b7b041578
Keshaw08 <keshawsoni08@gmail.com>
2021-06-06 20:04:51 +0530

    exer_java.java

--- exer_java.java
+++ exer_java.java
@@ -1246,3 +1246,14 @@ for(int i=10;i>0;i--) {
 			}
 			System.out.println();
 		}
+
+
+String arr1 [] = {"Hello", "Github", "in","JKLU"};
+		for(int i=0;i<arr1.length;i++) {
+			System.out.print(arr1[i]);
+			System.out.print("->s");
+			for(int j=0;j<arr1[i].length();j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
